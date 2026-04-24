41bcb7e1c65daed2b19f0a3ff0bd3dc3590ac0fa
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-24 15:41:14 -0400

    Lifted cart up and shared it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,18 +4,13 @@ import { Header } from "../components/Header";
 import "./HomePage.css";
 // save data
 // create hTML
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
-    axios.get('/api/cart-items')
-    .then((response) => {
-      setCart(response.data);
-
-    })
   }, []);
 
   return (
