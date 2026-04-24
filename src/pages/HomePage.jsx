1cb280ab95bd5ab531d8bbedb346ad0c3a1ed32b
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-24 15:22:14 -0400

    Used data from the backend to generate the HTML

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 // save data
 // create hTML
 export function HomePage() {
-
-  axios.get('http://localhost:3000/api/products')
-    .then((response) => {
-      console.log(response.data);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
     });
+  }, []);
 
   return (
     <>
@@ -40,7 +41,9 @@ export function HomePage() {
                   </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
