41505b2e9d519931a0b02093fca20767e322fb08
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-24 15:10:00 -0400

    Fetched data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import "./HomePage.css";
 // save data
 // create hTML
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
